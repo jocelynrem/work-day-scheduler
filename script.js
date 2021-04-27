d48dd150f1cd24cf5195393f4a036b9a4c5d25f1
jocelynrem <jocelynrem@gmail.com>
2021-04-26 20:08:30 -0400

    time css working, start of save funtion

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 
-  var currentDay = moment().format('dddd, MMMM DD, YYYY')
+  var currentDay = moment().format('dddd, MMMM DD, YYYY  h:mm a')
   $('#currentDay').text(currentDay)
+  var hour =  moment().format('H')
+  var timeBlock = $(".time-block");
+  var saveBtn = $(".saveBtn")
+  var event = $("description").innerText
+
+console.log('$("description").text():', $(".description").text())
+
+
+console.log(saveBtn.prev().text());
+
+// saveBtn.click(function() {
+//   $(this).prev().text()
+// })
+
+
+  function timeTracker(){
+    timeBlock.each(function(){
+    var thisBlock = $(this);
+    var thisBlockHr = parseInt(thisBlock.attr("data-hr"));
+  
+    if (thisBlockHr === hour) {
+      thisBlock.addClass("present").removeClass("past future");
+    }
+    if (thisBlockHr < hour) {
+      thisBlock.addClass("past").removeClass("present future");
+    }
+    if (thisBlockHr > hour) {
+      thisBlock.addClass("future").removeClass("past present");
+    }
+  });
+  }
+  timeTracker();
